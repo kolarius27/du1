955d0d3cd3f995ca023eef80d0f88ffde0dac5cf
Michal Kolář <michalkolar27@gmail.com>
2019-11-04 23:01:29 +0100

    Přidáno zaokrouhlení, oprava vzorce...

--- zobrazeni.py
+++ zobrazeni.py
@@ -1,9 +1,24 @@
 print("Vítejte v programu pro výpočet válcových tečných zobrazení!")
-z = float(input("Zadajte zobrazení: "))
+#z = float(input("Zadajte zobrazení: "))
 m = int(input("Zadejte měřítko: "))
-r = input("Zadejte poloměr Země (v km): ")
+r = float(input("Zadejte poloměr Země (v km): "))
 Rovnobezky = []
+Poledniky = []
+
+import math
 for i in range(19):
     v = int(-90 + i*10)
-    x = r*v
-
+    vrad = math.radians(v)
+    y = float(r*vrad*1000000/m)
+    yround = round(y,1)
+    tr = (v,yround)
+    Rovnobezky.append(tr)
+for k in range(37):
+    u = int(-180 + k*10)
+    urad = math.radians(u)
+    x = float(r*urad*1000000/m)
+    xround = round(x,1)
+    tp = (u,xround)
+    Poledniky.append(tp)
+print("Rovnobezky: ", Rovnobezky)
+print("Poledniky: ", Poledniky)
